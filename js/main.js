e369b56ac53b9533e9f5b0ff114c532d2f777dc9
Karen Heredia <karen.julieth.heredia@gmail.com>
2025-09-25 14:07:58 -0500

    Agregué el ejercicio de localStorage, empleando objetos en la práctica

--- js/main.js
+++ js/main.js
@@ -1,39 +1,58 @@
-const form = document.getElementById("formulario");
-const input = document.getElementById("nombre");
-const saludo = document.getElementById("saludo");
-const btnBorrar = document.getElementById("eliminar");
+const formCurso = document.getElementById("formularioCurso");
+const inputCurso = document.getElementById("cursos");
+const inputProfesor = document.getElementById("Profesor");
+const inputPrecio = document.getElementById("precio");
+const inputCiudad = document.getElementById("ciudad");
+const inputCupo = document.getElementById("cupo");
+const inputMensaje = document.getElementById("mensajeCurso");
+const btnBorrar = document.getElementById("borrar");
 
 
-form.addEventListener("submit", (e)=>{
+formCurso.addEventListener("submit", (e)=>{
     e.preventDefault();
-    const nombre = input.value.trim();
 
-    if (nombre == "") {
-        alert("Debe llenar el campo con su nombre");
+    const curso = inputCurso.value.trim();
+    const profesor = inputProfesor.value.trim();
+    const precio = inputPrecio.value.trim();
+    const ciudad = inputCiudad.value.trim();
+    const cupo = inputCupo.value.trim();
+   
+
+    if (curso == "" || profesor == "" || precio == "" || ciudad == "" || cupo == "") {
+        alert("Todos los campos son obligatorios");
         return;
     }
+//Anteriormente era un solo valor y ahora estamos alimentando un objeto con sus diferentes claves o valores
+    const nuevoCurso = {
+        nombre : curso,
+        profesor : profesor,
+        precio : precio,
+        ciudad : ciudad,
+        cupo : cupo
+    }
 
-    localStorage.setItem("usuario", nombre);
-    saludo.textContent = "Bienvenido " + nombre;
-    input.value = "";
+    localStorage.setItem("curso", JSON.stringify(nuevoCurso))
+
+    formCurso.reset()
 });
 
+document.addEventListener('DOMContentLoaded', ()=>{
+    const cursoCreado = localStorage.getItem("curso");
+    if (cursoCreado) {
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    const usuarioGuardado = localStorage.getItem("usuario");
+        const objetoCurso = JSON.parse(cursoCreado)
+        inputMensaje.innerHTML = "El curso es: " +  objetoCurso.nombre + "<br>" + "profesor: " + objetoCurso.profesor + "<br>" + "precio: " + objetoCurso.precio + "<br>" + "ciudad: " + objetoCurso.ciudad + "<br>" +  "cupo: " + objetoCurso.cupo; 
 
-    if (usuarioGuardado) {
-    saludo.textContent = "Bienvenido de nuevo " + usuarioGuardado;
-
+        
     }
 });
 
 
-btnBorrar.addEventListener("click", ()=>{
-    localStorage.removeItem("usuario");
-    saludo.textContent = "Bienvenido usuario indefinido";
 
-});
+
+
+
+
 
 
 
--- js/perfil.js
+++
@@ -1,6 +0,0 @@
-const userCreated = document.getElementById("usuarioCreado");
-
-document.addEventListener("DOMContentLoaded", ()=>{
-    const usuarioGuardado = localStorage.getItem("usuario");
-    userCreated.textContent = "Bienvenido de nuevo " + usuarioGuardado;
-});
