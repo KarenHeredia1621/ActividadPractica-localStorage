09c3a23c2b0077b493202156c379251fc1fe2131
Karen Heredia <karen.julieth.heredia@gmail.com>
2025-09-29 13:22:25 -0500

    “Realicé un función donde agrega cada curso a un arreglo, donde se agruparan todos los cursos. Luego mandé esa información a otro html donde lo organicé en una tabla, que  fue alimentada con la información del formulario” git push -u origin main
    cd . .
    cd ..
    ls

---
+++ js/mainArregloObjetos.js
@@ -0,0 +1,75 @@
+const formCurso = document.getElementById("formularioCurso");
+const inputCurso = document.getElementById("cursos");
+const inputProfesor = document.getElementById("Profesor");
+const inputPrecio = document.getElementById("precio");
+const inputCiudad = document.getElementById("ciudad");
+const inputCupo = document.getElementById("cupo");
+const inputMensaje = document.getElementById("mensajeCurso");
+const btnBorrar = document.getElementById("borrar");
+
+
+formCurso.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const curso = inputCurso.value.trim();
+    const profesor = inputProfesor.value.trim();
+    const precio = inputPrecio.value.trim();
+    const ciudad = inputCiudad.value.trim();
+    const cupo = inputCupo.value.trim();
+   
+
+    if (curso == "" || profesor == "" || precio == "" || ciudad == "" || cupo == "") {
+        alert("Todos los campos son obligatorios");
+        return;
+    }
+//Anteriormente era un solo valor y ahora estamos alimentando un objeto con sus diferentes claves o valores
+    const nuevoCurso = {
+        nombre : curso,
+        profesor : profesor,
+        precio : precio,
+        ciudad : ciudad,
+        cupo : cupo
+    }
+
+    //Capturamos el arreglo existente en el localStorage o lo creamos vacío si no existe previamente
+    const cursosGuardados = JSON.parse(localStorage.getItem("cursos"))||[];
+
+    //Agregamos al arreglo[] cursosGuardados el nuevoCurso{}
+    cursosGuardados.push(nuevoCurso);
+
+    localStorage.setItem("cursos", JSON.stringify(cursosGuardados))
+        const cursoCreado = localStorage.getItem("curso");
+
+        const objetoCurso = JSON.parse(cursoCreado)
+        inputMensaje.innerHTML = "El curso es: " +  objetoCurso.nombre + "<br>" + "profesor: " + objetoCurso.profesor + "<br>" + "precio: " + objetoCurso.precio + "<br>" + "ciudad: " + objetoCurso.ciudad + "<br>" +  "cupo: " + objetoCurso.cupo; 
+
+
+    formCurso.reset();
+});
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const cursoCreado = localStorage.getItem("curso");
+    if (cursoCreado) {
+
+        const objetoCurso = JSON.parse(cursoCreado)
+        inputMensaje.innerHTML = "El curso es: " +  objetoCurso.nombre + "<br>" + "profesor: " + objetoCurso.profesor + "<br>" + "precio: " + objetoCurso.precio + "<br>" + "ciudad: " + objetoCurso.ciudad + "<br>" +  "cupo: " + objetoCurso.cupo; 
+
+        
+    }
+});
+
+btnBorrar.addEventListener("click", ()=>{
+    localStorage.removeItem("cursos");
+    inputMensaje.textContent = "Cursos eliminado correctamente";
+
+});
+
+
+
+
+
+
+
+
+
+
